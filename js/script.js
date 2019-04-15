94395b10e7fdd0ac6aee825fb9ea3a04d9d8568b
khaque1 <khaque1@users.noreply.github.com>
2019-04-15 12:23:25 -0500

    add js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
  
-
-
-
-
-
-
+	var click = document.getElementById("entrybutton");
+	click.addEventListener("click", function () {
+		var msg = document.getElementById('entryinput').value;
+		alert("Kinza Haque: " + msg);
+		console.log(msg);
+		document.getElementById('textoutput').innerHTML = msg; 
+	});
+}
 
 window.addEventListener('load', init);
